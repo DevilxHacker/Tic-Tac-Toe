78ad20fd5b7efa41df401aaeac1e3cceed849d44
DevilxHacker <166166887+DevilxHacker@users.noreply.github.com>
2025-06-21 03:51:44 +0530

    Deployed

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'https://github.com/DevilxHacker/Tic-Tac-Toe',
 })
