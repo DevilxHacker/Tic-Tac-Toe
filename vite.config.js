417df90d05a0a685fcf0cf866859d6e90a954e1b
DevilxHacker <166166887+DevilxHacker@users.noreply.github.com>
2025-06-21 04:02:46 +0530

    Fixed base for Deployment

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://github.com/DevilxHacker/Tic-Tac-Toe',
+  base: /Tic-Tac-Toe/,
+
 })
